7b32563886b40d509eaa3415dacb00d3c0d244cc
Carlos Andres Aguilar Nieto <caguilar@ec.krugercorp.com>
2020-10-31 02:14:36 -0500

    first commit

---
+++ network/responde.js
@@ -0,0 +1,21 @@
+exports.success = function (res, res, message, status) {
+    let statusCode = status || 200;
+    let statusMessage = message || '';
+
+    res.status(statusCode).send({
+        error: false,
+        status: statusCode,
+        body: statusMessage
+    })
+}
+
+exports.error = function (res, res, message, status) {
+    let statusCode = status || 500;
+    let statusMessage = message || 'Internal Server Error';
+
+    res.status(statusCode).send({
+        error: false,
+        status: statusCode,
+        body: statusMessage
+    })
+}
