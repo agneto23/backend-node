0dbed4f38ccccf17e2ba1192db6249973c9eaa96
omar soriano <dost.soriano@gmail.com>
2020-10-31 07:35:50 +0000

    nuevo

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -5,7 +5,7 @@ const response = require('../../../network/response');
 const router = express.Router();
 
 router.get('/api/user', function (req, res) {
-    response.success(req, res, 'Todo correcto ok ok ok', 200)
+    response.success(req, res, 'Todo correcto ok ok ok oki', 200)
 })
 
 module.exports = router;
